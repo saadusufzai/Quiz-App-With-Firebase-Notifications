83c9a3672f7bbfd80d74572b992db933e4b14f2f
Saad Aslam <saadusufzai@gmail.com>
2020-09-07 02:09:04 +0500

    Create firebase-messaging-sw.js

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBaU7IuJxGZeGhIT3lao20PYqiGsvQmsYM",
+    authDomain: "quiz-app-push-notificati-10d11.firebaseapp.com",
+    databaseURL: "https://quiz-app-push-notificati-10d11.firebaseio.com",
+    projectId: "quiz-app-push-notificati-10d11",
+    storageBucket: "quiz-app-push-notificati-10d11.appspot.com",
+    messagingSenderId: "245030509701",
+    appId: "1:245030509701:web:685f8f66c27a92dc9d5dec",
+    measurementId: "G-NKV3G0F3SW"
+  };
